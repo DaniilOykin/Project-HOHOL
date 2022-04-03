3080960a4643cede10268ec82fb3aeace4306146
DaniilOykin <71172456+DaniilOykin@users.noreply.github.com>
2022-04-03 15:02:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-@name         New Userscript
-@namespace    http://tampermonkey.net/
-@version      0.1
-@description  try to take over the world!
-@author       You
-@match        https://www.isuct.ru/
-@icon         https://www.google.com/s2/favicons?sz=64&domain=soundsnap.com
-@grant        none
-
 (function() {
     'use strict';
     let imgs = document.querySelectorAll("img")
