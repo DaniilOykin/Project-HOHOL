0303caaaeb828f66915134c5fc2392350580a8aa
DaniilOykin <71172456+DaniilOykin@users.noreply.github.com>
2022-04-03 14:49:46 +0300

    1

---
+++ app.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.isuct.ru/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=soundsnap.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let imgs = document.querySelectorAll("img")
+    for(let img of imgs) {
+        img.src = "https://i.pinimg.com/originals/7a/da/30/7ada3098838344a39258f4618bce61f4.jpg"
+    }
+})();
