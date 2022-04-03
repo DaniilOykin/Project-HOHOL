96c8247104462905c247c579071cd266d21c234e
DaniilOykin <71172456+DaniilOykin@users.noreply.github.com>
2022-04-03 15:15:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  aboba
 // @author       You
 // @match        https://www.isuct.ru/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=soundsnap.com
