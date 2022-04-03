55f79ec9253ace236c982893fa8e1af5796714ce
DaniilOykin <71172456+DaniilOykin@users.noreply.github.com>
2022-04-03 15:29:53 +0300

    1

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Project HOHOL
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  aboba2
-// @author       You
+// @version      1
+// @description  rofl
+// @author       БыдлоSlayer
 // @match        *
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=soundsnap.com
-// @grant        none
 // ==/UserScript==
 
 (function() {
